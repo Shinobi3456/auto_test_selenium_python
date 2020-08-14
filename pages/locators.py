c1db544a46b2c66b09ce13761d1599ad1002f6aa
v_razvodov <shinobi3456@gmail.com>
2020-08-14 11:03:25 +0500

    add test product detail

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FROM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FROM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main>p.price_color")
+    NAME_PRODUCT = (By.TAG_NAME, "h1")
+    ALERT_SUCCESS = (By.CSS_SELECTOR, ".alert-success strong")
+    ALERT_BASKET = (By.CSS_SELECTOR, ".alert-info strong")
+    
+
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    
+    def add_to_basket(self):
+        bnt_add_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        bnt_add_basket.click()
+    
+    def should_be_success_message(self):
+        alerts = self.browser.find_elements(*ProductPageLocators.ALERT_SUCCESS)
+        name_product_tag = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT)
+        name = name_product_tag.text
+        
+        result = False
+        for alert in alerts:
+            if name == alert.text:
+                result = True
+                break
+        
+        assert result is True, 'The name of the product added to the cart does not match the name on the product card'
+    
+    def should_be_basket_price(self):
+        alert = self.browser.find_element(*ProductPageLocators.ALERT_BASKET)
+        product_price_teg = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT)
+        
+        product_price = product_price_teg.text
+        basket_price = alert.text
+        
+        assert product_price == basket_price, f'Price product {product_price} != Basket price {basket_price}'
